facf3fd2479ae491ce026d57dddc9345fb467de9
S. Halici <122233382+9winged@users.noreply.github.com>
2025-04-09 14:46:37 +0200

    meow

--- Skripte/manipulation/0.sql
+++ Skripte/manipulation/0.sql
@@ -1,15 +1,3 @@
 cl scr
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 @&pfad\menue\hauptmenue.sql
-
-
-
-=======
-@C:\datenbank\Biblioteca\Skripte\menue\hauptmenue.sql
->>>>>>> origin/main
-=======
-@C:\datenbank\Biblioteca\Skripte\menue\hauptmenue.sql
->>>>>>> 64e42ad37e1ef2e37d6fdd7063c518e9f1bde7e6
-
